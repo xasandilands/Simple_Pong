ad6bedf45bfb6f402022063aaa87df43e40210e6
Xavier <njsandilands@gmail.com>
2025-08-20 16:28:22 -0500

    Update Main.cpp

--- simple_pong/Main.cpp
+++ simple_pong/Main.cpp
@@ -5,9 +5,19 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include"Game.h"
+#include"Shader.h"
+#include"Texture.h"
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
 //window width and height
-int width = 800;
-int height = 600;
+const unsigned int width = 800;
+const unsigned int height = 600;
+
+Game Pong(width, height);
 
 int main()
 {
@@ -36,20 +46,58 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	//set viewport
 	glViewport(0, 0, width, height);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	Pong.Activate();
+
+
+	float deltaTime = 0.0f;
+	float prevFrame = 0.0f;
 	//keeps program and wimdow running/open until user closes
 	while (!glfwWindowShouldClose(window))
 	{
+		float currFrame = glfwGetTime();
+		deltaTime = currFrame - prevFrame;
+		prevFrame = currFrame;
+		glfwPollEvents();
+
+		Pong.Inputs(deltaTime);
+		Pong.Update(deltaTime);
+
 		//bg color, clear color buffer
 		glClearColor(0.2f, 0.0f, 0.7f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		//swap back buffer to front
+		Pong.Render();
 		glfwSwapBuffers(window);
-		glfwPollEvents();
 	}
 	//terminate program
 	glfwDestroyWindow(window);
 	glfwTerminate();
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwWindowShouldClose(window) == true;
+	}
+
+	if (key >= 0 && key <= 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			Pong.Keys[key] = true;
+		}
+		else if(action == GLFW_RELEASE)
+		{
+			Pong.Keys[key] = false;
+		}
+	}
 }
